7ca2d43de7ed9628c92e1306b8fe910663eac798
amyseol <141234899+amyseol@users.noreply.github.com>
2024-04-02 00:36:42 +0900

    Title: 윤년, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

--- 240402/윤년/leap-year.java
+++ 240402/윤년/leap-year.java
@@ -1,20 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-		
-		int year = in.nextInt();
-		in.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(br.readLine());
 
         if(year%4==0){
             if(year%400==0){
                 System.out.println(1);
-            }else if(year%100!=0){
+            }else if(year%100==0){
                 System.out.println(0);
-            }
-        }else{
-            System.out.println(0);
-        }
+            }else{System.out.println(1);}
+        }else{System.out.println(0);}
     }
 }
