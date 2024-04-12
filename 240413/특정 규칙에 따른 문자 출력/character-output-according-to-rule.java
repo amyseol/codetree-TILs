cf9ba1151d74e3366ca93039652558c2cedce963
amyseol <141234899+amyseol@users.noreply.github.com>
2024-04-13 01:03:59 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240413/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // n= 3
+
+        for(int i=0; i<n; i++){ // i= 0,1,2
+            for(int j=0; j<n-i-1; j++){ // 2,1,0
+                System.out.print("  ");
+            }
+            for(int j=0; j<i+1; j++){ // 1,2,3
+                System.out.print("@ ");
+            }
+            System.out.println();
+        }
+
+        for(int i=0; i<n-1; i++){ // i= 0,1
+            for(int j=0; j<n-i-1; j++){ // 2,1
+                System.out.print("@ ");
+            }
+            for(int j=0; j<i+1; j++){ // 1,2
+                System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
