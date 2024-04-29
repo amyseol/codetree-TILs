f13ac389183ad3effcd311c29c9e66feaae39402
amyseol <141234899+amyseol@users.noreply.github.com>
2024-04-29 16:12:22 +0900

    Title: 학점 계산기, Time: 156ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240429/학점 계산기/credit-calculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+
+        double[] arr = new double[n];
+        double sum = 0;
+
+        String result = "";
+
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextDouble();
+            sum += arr[i];
+        }
+        double avg = sum/n;
+        if(avg >= 4.0){
+            result = "Perfect";
+        }else if(avg >= 3.0){
+            result = "Good";
+        }else{
+            result = "Poor";
+        }
+        System.out.printf("%.1f%n", avg);
+        System.out.print(result);
+    }
+}
