0f9ce7731f8950b5468ffa6b4130e487b76db8b7
amyseol <141234899+amyseol@users.noreply.github.com>
2024-05-03 17:18:19 +0900

    Title: 전항의 두 배, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240503/전항의 두 배/twice-the-previous.java
+++ 240503/전항의 두 배/twice-the-previous.java
@@ -2,14 +2,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int arr[] = new int[2];
-        for(int i = 0; i < 2; i++){
-            arr[i] = sc.nextInt();
-        }
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
 
         int newArr[] = new int[10];
-        newArr[0] = arr[0];
-        newArr[1] = arr[1];
+        newArr[0] = n1;
+        newArr[1] = n2;
         
         for(int i = 2; i < 10; i++){
             newArr[i] = newArr[i-1] + newArr[i-2] * 2;
